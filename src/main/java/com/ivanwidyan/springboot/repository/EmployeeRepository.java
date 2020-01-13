506af1278d820078af9f4ee5c7a1539dba9acfbb
Ivan Widyan <ivan.widyan@gdn-commerce.com>
2020-01-14 00:18:28 +0700

    Initial commit

---
+++ src/main/java/com/ivanwidyan/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.ivanwidyan.springboot.repository;
+
+import com.ivanwidyan.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
