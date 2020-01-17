77eb36f045aaf69bd0caacba6cd8dfccfbb9bd3f
shirokuma-ds <59801616+shirokuma-ds@users.noreply.github.com>
2020-01-17 15:31:01 +0700

    Add update & delete controller, search using id Add add member controller

--- src/main/java/com/ivanwidyan/springboot/controller/MemberController.java
+++ src/main/java/com/ivanwidyan/springboot/controller/MemberController.java
@@ -10,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -36,4 +39,42 @@ public class MemberController {
                 .orElseThrow(() -> new ResourceNotFoundException("Member not found for this id :: " + id));
         return ResponseEntity.ok().body(employee);
     }
+
+    @ApiOperation(value = "Add a member")
+    @PostMapping("/register/member")
+    public Member createMember(
+            @ApiParam(value = "Save member data to the database", required = true) @Valid @RequestBody Member member) {
+        return memberRepository.save(member);
+    }
+
+    @ApiOperation(value = "Update a member")
+    @PutMapping("/update/member")
+    public ResponseEntity<Member> updateMember(
+            @ApiParam(value = "Member Id used to update specific member", required = true) @RequestParam Long id,
+            @ApiParam(value = "Data for the updated member", required = true) @Valid @RequestBody Member memberDetails)
+            throws ResourceNotFoundException {
+
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Member not found for this id: " + id));
+        member.setName(memberDetails.getName());
+        member.setEmail(memberDetails.getEmail());
+        member.setPhoneNumber(memberDetails.getPhoneNumber());
+        final Member updatedMember = memberRepository.save(member);
+        return ResponseEntity.ok(member);
+    }
+
+    @ApiOperation(value = "Delete a member")
+    @DeleteMapping("/delete/member")
+    public Map<String, Boolean> deleteMember(
+            @ApiParam(value = "Member id used to delete specific member", required = true) @RequestParam Long id)
+            throws ResourceNotFoundException {
+
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Member not found for this id: " + id));
+        memberRepository.delete(member);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", true);
+        return  response;
+    }
+
 }
